99ca298302362f6b4262eaee9cbb0e8f71cc7266
tombetthauser <tombetthauser@gmail.com>
2021-01-09 14:37:54 -0800

    Ran update

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const directories = [
 './ca-rock-garden/index.html',
 './cave-01/index.html',
 './tuesday-test/index.html',
+'./water-burger/index.html',
 ];
 window.location.href = directories[Math.floor(Math.random() * directories.length)];
