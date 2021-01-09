e0252d86b974a180f460d54b422b254b52bf5a4a
tombetthauser <tombetthauser@gmail.com>
2021-01-09 01:58:30 -0800

    Update script

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const directories = [
 './ca-rock-garden/index.html',
+'./cave-01/index.html',
 './tuesday-test/index.html',
 ];
 window.location.href = directories[Math.floor(Math.random() * directories.length)];
